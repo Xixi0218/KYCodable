8b9a5bff1ae60657c631fc72c86a91b878b3f4d2
keyon.ye <keyon.ye@ponyft.com>
2022-09-05 09:34:44 +0800

    Initial Commit

---
+++ KYCodable/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KYCodable
+//
+//  Created by keyon on 2022/9/5.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
